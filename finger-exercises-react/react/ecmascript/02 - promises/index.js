7e32026aacd6b866311dd0b0b44af6c8f510771e
franguerini <francisco.guerini@wolox.com.ar>
2018-05-11 16:40:44 -0300

    First commit promise test passing

--- finger-exercises-react/react/ecmascript/02 - promises/index.js
+++ finger-exercises-react/react/ecmascript/02 - promises/index.js
@@ -1,9 +1,9 @@
 // Hint: use setInterval, create a new Promise and measure time with Date.now()
 
-export function delay() {
-
+export function delay(time) {
+  return Promise.resolve(time);
 }
 
-export function asyncDelay() {
-
+export function asyncDelay(time) {
+  return Promise.resolve(time);
 }
