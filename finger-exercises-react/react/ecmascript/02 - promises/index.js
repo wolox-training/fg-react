af7af5ac9b179af56ddc9b646ce4310703b8ccf2
franguerini <francisco.guerini@wolox.com.ar>
2018-05-15 14:16:08 -0300

    finger excercise react

--- finger-exercises-react/react/ecmascript/02 - promises/index.js
+++ finger-exercises-react/react/ecmascript/02 - promises/index.js
@@ -1,9 +1,28 @@
 // Hint: use setInterval, create a new Promise and measure time with Date.now()
 
 export function delay(time) {
-  return Promise.resolve(time);
+  return new Promise(((resolve, reject) => {
+    if (time > 2000) {
+      reject(new Error('This time is too much !'));
+    }
+    const timeEnd = Date.now() + time;
+    setInterval(() => {
+      if (timeEnd < Date.now()) {
+        resolve(time);
+      }
+    }, 50);
+  }));
 }
 
 export function asyncDelay(time) {
-  return Promise.resolve(time);
+  const makeRequest = async () => {
+    const timeEnd = Date.now() + 500;
+    await setInterval((resolve) => {
+      if (timeEnd < Date.now()) {
+        resolve(time);
+      }
+    }, 50);
+    return time;
+  };
+  return makeRequest();
 }
